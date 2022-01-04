a664661ff70cc40dbddd2eb0bbfc8f00f9728213
cookieGon <jygon931203@gmail.com>
2022-01-04 15:25:09 +0900

    first commit

---
+++ baseballGame/src/application/Game.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Game {
+	public void play() {
+		double q1 = Math.floor(Math.random() * 9);
+		double q2 = Math.floor(Math.random() * 9);
+		double q3 = Math.floor(Math.random() * 9);
+		if(q1 == q2) {
+			q2 = Math.floor(Math.random() * 9);
+		}
+		System.out.println(q1);
+		System.out.println(q2);
+		System.out.println(q3);
+	}
+}
