e765f1b65adf92d777491fb2cf42365c7fbda53b
cookieGon <jygon931203@gmail.com>
2022-01-05 15:33:00 +0900

    1월5일 업데이트

--- baseballGame/src/application/Game.java
+++ baseballGame/src/application/Game.java
@@ -5,11 +5,9 @@ public class Game {
 		double q1 = Math.floor(Math.random() * 9);
 		double q2 = Math.floor(Math.random() * 9);
 		double q3 = Math.floor(Math.random() * 9);
-		if(q1 == q2) {
+		if (q1 == q2) {
 			q2 = Math.floor(Math.random() * 9);
 		}
-		System.out.println(q1);
-		System.out.println(q2);
-		System.out.println(q3);
+		System.out.println(q1 + " " + q2 + " " + q3);
 	}
 }
