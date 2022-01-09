a78a4860cb8cb617526fb3501798c32e2bfd6a89
cookieGon <jygon931203@gmail.com>
2022-01-09 15:12:15 +0900

    1월9일 업데이트

--- baseballGame/src/application/Game.java
+++ baseballGame/src/application/Game.java
@@ -6,7 +6,11 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Game {
-	private int q1, q2, q3;
+	String[] arr = new String[3];
+
+	ArrayList<String> al = new ArrayList<>();
+
+	String q1, q2, q3;
 
 	public void play() {
 		setNum();
@@ -14,23 +18,21 @@ public class Game {
 
 	public void setNum() {
 		while (true) {
-			q1 = (int) Math.floor(Math.random() * 9);
-			q2 = (int) Math.floor(Math.random() * 9);
-			q3 = (int) Math.floor(Math.random() * 9);
+			q1 = String.valueOf((int) Math.floor(Math.random() * 9));
+			q2 = String.valueOf((int) Math.floor(Math.random() * 9));
+			q3 = String.valueOf((int) Math.floor(Math.random() * 9));
 			if (q1 != q2 && q1 != q3 && q2 != q3) {
 				break;
 			} else {
-				q2 = (int) Math.floor(Math.random() * 9);
-				q3 = (int) Math.floor(Math.random() * 9);
+				q2 = String.valueOf((int) Math.floor(Math.random() * 9));
+				q3 = String.valueOf((int) Math.floor(Math.random() * 9));
 			}
 		}
-		ArrayList<Integer> al = new ArrayList<>();
 		al.add(q1);
 		al.add(q2);
 		al.add(q3);
-		System.out.println(al.get(1));
-		for(Integer li : al) {
-			System.out.print(li+" ");
+		for (String li : al) {
+			System.out.print(li + " ");
 		}
 		System.out.println();
 		getNum();
@@ -42,12 +44,30 @@ public class Game {
 		try {
 			System.out.print("숫자 입력 (a b c) : ");
 			as = br.readLine();
-			String arr[] = as.split(" ");
-			for (String str : arr) {
-				System.out.println(str);
-			}
+			arr = as.split(" ");
+//			for (String str : arr) {
+//				System.out.println(str);
+//			}
+			System.out.println(arr[1]);
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("숫자 3개를 입력해주세요!");
+		} finally {
+			int ball = ballCount();
+			System.out.println(ball);
 		}
+	}
+
+	public int ballCount() {
+		int count = 0;
+		for (String li : al) {
+			for (String str : arr) {
+				if (li == str) {
+					count++;
+				}
+			}
+		}
+		return count;
 	}
 }
